a7c9094fd38c60176ef3fc43191ffc3ebd312316
AlonsoMD <alonso2295@hotmail.com>
2018-04-26 00:18:04 -0500

    lastcommit

--- pc2/src/main/java/com/progra/pc2/controllers/TributoController.java
+++ pc2/src/main/java/com/progra/pc2/controllers/TributoController.java
@@ -1,5 +1,8 @@
 package com.progra.pc2.controllers;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,19 +25,117 @@ public class TributoController {
 	@Autowired
 	private TributoRepository tributoRepository;
 	
-	public Person logicaNegocio(Person person)
+	public Person LogicaNegocio(Person person)
 	{
+		double impuestoTotal = 0;
+		double [] montoAfectado = {0,0,0,0,0};
+		double [] impuestoAfectado = {0,0,0,0,0};
 		String tipoTrabajador = person.getTipoTrabajador();
 		double salarioMensual = person.getSalario();
+		double uit = person.getUit();
 		
 		double salarioAnual = 0;
 		
-		salarioAnual = (tipoTrabajador=="Independiente")?salarioMensual*12:salarioMensual*14;
+		if(tipoTrabajador.equalsIgnoreCase("Independiente"))
+		{
+			salarioAnual = salarioMensual * 12;
+		}else
+		{
+			salarioAnual = salarioMensual * 14;
+		}
+
 		
+		person.setTotalBruto(round(salarioAnual,2));
 		
+		double descuento7uit = CalcularDescuento7UIT(person.getUit());
 		
+		double renta_neta = salarioAnual - descuento7uit;
+		
+		double [] montos_maximos = {5*uit,20*uit,35*uit, 45*uit};
+		
+		if (renta_neta > 0)
+		{
+			if(renta_neta<= montos_maximos[0])
+			{
+				montoAfectado[0] = renta_neta;
+				impuestoAfectado[0] = montoAfectado[0] * tasas[0];
+			}else
+			{
+				montoAfectado[0] = montos_maximos[0];
+				impuestoAfectado[0] = montoAfectado[0] * tasas[0];
+			}
+			
+			if(renta_neta <= montos_maximos[1])
+			{
+				montoAfectado[1] = renta_neta - montoAfectado[0];
+				impuestoAfectado[1] = montoAfectado[1] * tasas[1];
+			}else
+			{
+				montoAfectado[1] = montos_maximos[1] - montoAfectado[0];
+				impuestoAfectado[1] = montoAfectado[1] * tasas[1];
+			}
+			
+			if(renta_neta <= montos_maximos[2])
+			{
+				montoAfectado[2] = renta_neta - (montoAfectado[0]+montoAfectado[1]);
+				impuestoAfectado[2] = montoAfectado[2] * tasas[2];
+			}else
+			{
+				montoAfectado[2] = montos_maximos[2] - (montoAfectado[0]+montoAfectado[1]);
+				impuestoAfectado[2] = montoAfectado[2] * tasas[2];
+			}
+			
+			if(renta_neta <= montos_maximos[3])
+			{
+				montoAfectado[3] = renta_neta - (montoAfectado[0]+montoAfectado[1]+montoAfectado[2]);
+				impuestoAfectado[3] = montoAfectado[3] * tasas[3];
+			}else
+			{
+				montoAfectado[3] = montos_maximos[3] - (montoAfectado[0]+montoAfectado[1]+montoAfectado[2]);
+				impuestoAfectado[3] = montoAfectado[3] * tasas[3];
+			}
+			
+			if(renta_neta > montos_maximos[3])
+			{
+				montoAfectado[4] = renta_neta - montos_maximos[3];
+				impuestoAfectado[4] = montoAfectado[4] * tasas[4];
+			}else
+			{
+				montoAfectado[4] = 0;
+				impuestoAfectado[4] = montoAfectado[4] * tasas[4];
+			}
+		}
+		
+		impuestoTotal = CalcularImpuestoTotal(impuestoAfectado);
+		
+		person.setTotalImpuesto(round(impuestoTotal,2));
+
 		
 		return person;
+	}
+	
+	public double CalcularImpuestoTotal(double [] impuesto)
+	{
+		double impuestoTotal = 0;
+		for(int i=0; i < impuesto.length; i++)
+		{
+			impuestoTotal += impuesto[i];
+		}
+		return impuestoTotal;
+				
+	}
+	public double CalcularDescuento7UIT(double uit)
+	{
+		return uit * 7;
+	}
+	
+	private double round(double valor, int q_decimales) {
+	    if (q_decimales < 0) return 0;
+
+	    long factor = (long) Math.pow(10, q_decimales);
+	    valor = valor * factor;
+	    long tmp = Math.round(valor);
+	    return (double) tmp / factor;
 	}
 	
 	
@@ -52,8 +153,18 @@ public class TributoController {
 			return "formulario";
 		}
 		
-		Person _person = logicaNegocio(person);
+		Person _person = LogicaNegocio(person);
 		
-		return "formulario";
+		tributoRepository.save(_person);
+		
+		return "redirect:/persona/listado";
+	}
+	
+	@GetMapping("/persona/listado")
+	private String listado(Map<String, Object> model) {
+		
+		List<Person> personas = tributoRepository.findAll();
+		model.put("personas", personas);
+		return "listPerson";
 	}
 }
